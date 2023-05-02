8fd325ddecd4b3e5ac27b93f7c83d8466baf47ab
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2023-05-02 20:12:16 +0600

    final commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#top').fadeIn();
+    } else {
+      $('#top').fadeOut();
+    }
+  });
+      
+  $('#top').click(function() {
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
+  });
+});
+    
